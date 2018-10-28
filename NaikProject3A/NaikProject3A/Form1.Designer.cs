5f6adf0c6fb70055d690ce69bbbf522d62711201
naikk84 <42657070+naikk84@users.noreply.github.com>
2018-10-28 12:16:11 -0600

    Add files via upload

---
+++ NaikProject3A/NaikProject3A/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace NaikProject3A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.First1 = new System.Windows.Forms.MaskedTextBox();
+            this.Second2 = new System.Windows.Forms.MaskedTextBox();
+            this.Result = new System.Windows.Forms.MaskedTextBox();
+            this.butCalculate = new System.Windows.Forms.Button();
+            this.butExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Second No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(204, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "/";
+            // 
+            // First1
+            // 
+            this.First1.Location = new System.Drawing.Point(167, 33);
+            this.First1.Name = "First1";
+            this.First1.Size = new System.Drawing.Size(100, 20);
+            this.First1.TabIndex = 0;
+            // 
+            // Second2
+            // 
+            this.Second2.Location = new System.Drawing.Point(167, 105);
+            this.Second2.Name = "Second2";
+            this.Second2.Size = new System.Drawing.Size(100, 20);
+            this.Second2.TabIndex = 1;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(167, 166);
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(100, 20);
+            this.Result.TabIndex = 7;
+            this.Result.TabStop = false;
+            // 
+            // butCalculate
+            // 
+            this.butCalculate.Location = new System.Drawing.Point(65, 224);
+            this.butCalculate.Name = "butCalculate";
+            this.butCalculate.Size = new System.Drawing.Size(75, 23);
+            this.butCalculate.TabIndex = 2;
+            this.butCalculate.Text = "Calculate";
+            this.butCalculate.UseVisualStyleBackColor = true;
+            this.butCalculate.Click += new System.EventHandler(this.butCalculate_Click);
+            // 
+            // butExit
+            // 
+            this.butExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.butExit.Location = new System.Drawing.Point(167, 224);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(75, 23);
+            this.butExit.TabIndex = 3;
+            this.butExit.Text = "Exit";
+            this.butExit.UseVisualStyleBackColor = true;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.butCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.butExit;
+            this.ClientSize = new System.Drawing.Size(390, 279);
+            this.Controls.Add(this.butExit);
+            this.Controls.Add(this.butCalculate);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Second2);
+            this.Controls.Add(this.First1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox First1;
+        private System.Windows.Forms.MaskedTextBox Second2;
+        private System.Windows.Forms.MaskedTextBox Result;
+        private System.Windows.Forms.Button butCalculate;
+        private System.Windows.Forms.Button butExit;
+    }
+}
+
---
+++ NaikProject3A/NaikProject3A/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaikProject3A
+{ 
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class startwithfive : Exception
+        {
+
+
+            public startwithfive()
+            {
+                MessageBox.Show("An exception has occured the number cannot start with 5 for the first number");
+            }
+        }
+        private void butCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string a = First1.Text;
+                decimal Number1 = Convert.ToDecimal(First1.Text);
+                decimal Number2 = Convert.ToDecimal(Second2.Text);
+                decimal number3 = Number1 / Number2;
+                if (Number1 > 8000)
+                {
+                    throw new OverflowException();
+                };
+                if (a.StartsWith("5"))
+                {
+                    throw new startwithfive();
+                }
+                Result.Text = number3.ToString();
+                First1.Focus();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Format Exception has occured enter values in numbers only");
+                First1.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow exception has occured enter First Number less then 8000");
+                First1.Focus();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Divide by zero exception has occured enter a number greater than zero for Second Number");
+                Second2.Focus();
+            }
+            catch (startwithfive)
+            {
+                First1.Focus();
+            }
+
+        }
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
